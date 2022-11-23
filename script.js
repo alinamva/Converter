bfed6d6ce96c0aadc9e000873c4da1699a5d845f
Alina Mamedova <alinamvaa@mail.ru>
2022-11-23 11:41:05 +0400

    js2

--- script.js
+++ script.js
@@ -1,31 +1,133 @@
 let firstNum = document.querySelector('.first-num')
 let secondNum = document.querySelector('.second-num')
-let firstTd = document.querySelectorAll('.first-block td')
-let secondTd = document.querySelectorAll('.second-block td')
+let firstTd = document.querySelectorAll('.first-block button')
+let secondTd = document.querySelectorAll('.second-block button')
+const api ='https://api.exchangerate-api.com/v4/latest/USD'
+let convertFromInfoText = document.querySelector(".from-text");
+let convertToInfoText = document.querySelector(".to-text");
+let li = document.querySelector('li')
+let resultFrom;
+let resultTo;
+
+firstNum.addEventListener('input', (e) => {
+    e.target.value = e.target.value.split(',').join('.');
+});
+secondNum.addEventListener('input', (e) => {
+    e.target.value = e.target.value.split(',').join('.');
+});
 
 
-function active() {
-    firstTd.forEach(elements => {
-        elements.addEventListener('click', () => {
-            firstTd.forEach(x => x.classList.remove('active'));
-            elements.classList.add('active');
-        })
-    })
-    secondTd.forEach(elements => {
-        elements.addEventListener("click", () => {
-            secondTd.forEach(x => x.classList.remove('active'));
-            elements.classList.add('active');
-        })
-    })
-};
-active();
-
-let currency1 = firstNum.value;
-window.innerHTML =  currency1
+let convertFromCurrency = 'USD';
+let convertToCurrency = 'TRY';
+let convertFromUSD = document.querySelector("div.from > button.usd");
+let convertFromTRY = document.querySelector("div.from > button.try");
+let convertFromEUR = document.querySelector("div.from > button.eur");
+let convertFromGBP = document.querySelector("div.from > button.gbp");
 
 
+let convertToUSD = document.querySelector("div.to > button.usd");
+let convertToTRY = document.querySelector("div.to > button.try");
+let convertToEUR = document.querySelector("div.to > button.eur");
+let convertToGBP = document.querySelector("div.to > button.gbp");
 
-
-
+let allConvertFroms = [convertFromUSD, convertFromTRY, convertFromEUR, convertFromGBP];
+let allConvertTos = [convertToUSD, convertToTRY, convertToEUR, convertToGBP];
 
     
+let url = `https://api.exchangerate.host/latest?base=${convertFromCurrency}&symbols=${convertToCurrency}`;
+
+convertFromUSD.style.backgroundColor = '#833AE0';
+convertFromUSD.style.color = 'white';
+convertToTRY.style.backgroundColor = '#833AE0';
+convertToTRY.style.color = 'white';
+
+function convert(firstNum, secondNum) {
+    firstNum.addEventListener('input', () => {
+
+        if (!isNaN(firstNum.value)) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    secondNum.value = data.rates[convertToCurrency] * firstNum.value;
+                });
+    
+        } else {
+            firstNum.value = 0;
+        }
+
+    });
+}
+convert(firstNum,secondNum);
+
+function converter(firstNum, secondNum) {
+    secondNum.addEventListener('input', () => {
+
+        if (!isNaN(secondNum.value)) {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    firstNum.value = data.rates[convertToCurrency] * secondNum.value;
+                });
+    
+        } else {
+            secondNum.value = 0;
+        }
+
+    });
+}
+
+converter(firstNum, secondNum);
+
+allConvertFroms.forEach((element) => {
+   
+    element.addEventListener('click', () => {
+
+        element.style.backgroundColor = '#833AE0';
+        element.style.color = 'white';
+
+        for(a in allConvertFroms) {
+            if (allConvertFroms[a] !== element) {
+                allConvertFroms[a].style.backgroundColor = 'white';
+                allConvertFroms[a].style.color = 'rgb(163, 163, 163)';
+            }
+        }
+        
+
+        convertFromCurrency = element.innerText;
+ url = `https://api.exchangerate.host/latest?base=${convertFromCurrency}&symbols=${convertToCurrency}`;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+             secondNum.value =  data.rates[convertToCurrency] * firstNum.value;
+      convertFromInfoText.innerText = `1 ${convertFromCurrency} = ${data.rates[convertToCurrency]} ${convertToCurrency}`;
+                convertToInfoText.innerText = `1 ${convertToCurrency} = ${data.rates[convertToCurrency]} ${convertFromCurrency}`;
+
+            })
+    });
+});
+
+allConvertTos.forEach((element) => {
+    element.addEventListener('click', () => {
+        element.style.backgroundColor = '#833AE0';
+        element.style.color = 'white';
+
+        for(a in allConvertTos) {
+            if (allConvertTos[a] !== element) {
+                allConvertTos[a].style.backgroundColor = 'white';
+                allConvertTos[a].style.color = 'rgb(163, 163, 163)';
+            }
+        }
+
+        convertToCurrency = element.innerText;     
+        url = `https://api.exchangerate.host/latest?base=${convertFromCurrency}&symbols=${convertToCurrency}`;
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                firstNum.value = data.rates[convertToCurrency] * secondNum.value;
+      convertToInfoText.innerText = `1 ${convertToCurrency} = ${data.rates[convertFromCurrency.value]} ${convertFromCurrency}`;
+                convertFromInfoText.innerText = `1 ${convertFromCurrency} = ${data.rates[convertToCurrency]} ${convertToCurrency}`;
+            })
+            converter(firstNum, secondNum);
+    })
+});
